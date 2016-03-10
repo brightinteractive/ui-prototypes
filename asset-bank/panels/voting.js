0ced11473663aed6d1a9a8c2434a299400c63393
Ben Browning <ben@bright-interactive.co.uk>
2016-03-10 17:00:31 +0000

    Adding voting functionality

--- asset-bank/panels/voting.js
+++ asset-bank/panels/voting.js
@@ -2,11 +2,39 @@ var voting = (function() {
 	'use strict';
 
 	function init() {
-		$('.js-vote').on('click', function() {
 
-			console.log
+		// Check local storage for whether user has voted
+		if (hasUserVoted()) {
+			disableVotes();
+		}
+
+		$('.js-vote-btn').on('click', function(evt) {
+			evt.preventDefault();
+
+			//check user hasn't already voted 
+			if (!hasUserVoted()) {
+				var voteOption = $('evt.currentTarget').data('vote');
+				ga('send', 'event', 'Panel Voting', voteOption);
+
+				console.log('voted');
+
+				// store that user has voted 
+				localStorage.setItem('hasVoted', true);
+
+				disableVotes();
+			}
+			
 		});	
 	}
+
+	function disableVotes() {
+		$('.js-vote').addClass('has-voted');
+	}
+
+	function hasUserVoted() {
+		return localStorage.getItem('hasVoted');
+	}
+
 
 	return {
 		init:init
