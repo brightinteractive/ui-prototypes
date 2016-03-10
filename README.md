bb3b5fd9e369fd97fcece2e2163ae3dd8d7ed0ce
Ben Browning <ben@bright-interactive.co.uk>
2016-03-10 16:19:52 +0000

    Add analytics
    And some other simple css tweaks

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ui-prototypes
+
+
+If you need to track how people are using your prototype, then I have set up an analytics account for the site.
+Tracking ID = UA-349139-12
---
+++ asset-bank/panels/voting.js
@@ -0,0 +1,14 @@
+var voting = (function() {
+	'use strict';
+
+	function init() {
+		$('.js-vote').on('click', function() {
+
+			console.log
+		});	
+	}
+
+	return {
+		init:init
+	};
+}());
