bee7d4eb2975ca010f0bd9b14611febd19974f60
Ben Browning <ben@bright-interactive.co.uk>
2019-01-22 16:45:23 +0000

    Further work on tagging prototype
    Get jquery autocomplete hooked up.
    Styling imrpovements
    Finish off the click to edit stuff.

--- tagging/tagging-behaviour.js
+++ tagging/tagging-behaviour.js
@@ -3,35 +3,82 @@ var tagInput = new Taggle('tagInput', {
     tags: ['Landscape', 'Trees', 'Pine'],
     preserveCase: true,
     additionalTagClasses: 'js-tag tag',
-    focusInputOnContainerClick: false,
-    tagFormatter: constructTagElementToDisplay
+    focusInputOnContainerClick: true,
+    tagFormatter: constructTagElementToDisplay,
+    onTagAdd: clearTheInput,
+    duplicateTagClass: 'is-duplicate',
+    onBeforeTagAdd: removeDuplicateClasses
 });
 
+
+function clearTheInput() {
+    tagInput.getInput().value = "";
+}
+
 $('body').on('click', function(evt) {
-    if(!$(evt.target).parents('js-edit-form')) {
-        stopEditingAllTags($(evt.target).parents('.js-tags'));
-    }  
+    var clickIsFromATag = $(evt.target).parents('.js-tag-edit-form').length | $(evt.target).hasClass('js-tag-text');
+    if(!clickIsFromATag) {
+        stopEditingAllTags();
+    }
 });
 
 $('.js-tags').on('click', function(evt) {
-    
     if($(evt.target).hasClass('js-tag-text')) {
         var $tag = getParentTag(evt.target);
         startEditingTag($tag);
     }       
-
 });
 
-$('.js-tag-edit-form').on('submit', function(evt) {
+var availableTags = [
+    'apple', 
+    'banana', 
+    'orange', 
+    'pear', 
+    'strawberry', 
+    'peach',
+    'cranberry',
+    'melon',
+    'plum',
+    'grape' 
+  ];
+
+$('.js-tags').on('submit',' .js-tag-edit-form', function(evt) {
     evt.preventDefault();
     var $tag = getParentTag(evt.target);
-    var newValue = $(evt.target).find('.js-tag-edit-input').val();
+    var newValue = $(evt.target).find('.js-tag-edit-input').text();
     updateTagValue($tag, newValue);
     $tag.removeClass('is-editing');
 });
 
+
+
+var container = tagInput.getContainer();
+
+
+$(tagInput.getInput()).autocomplete({
+    source: availableTags,
+    open: function() {
+        $("ul.ui-menu").width( $(this).parents('.js-tags').innerWidth() );
+    },
+    position: { my: "left top", at: "left bottom", collision: "none", of: container },
+    select: function( event, data ) {
+        
+        event.preventDefault();
+        
+        //Add the tag if user clicks
+        if (event.which === 1) {
+            tagInput.add(data.item.label);
+        }
+    }
+
+});
+
+
 function getEditFormHTML() {
-    return $('<form class="tag__edit js-tag-edit-form"><input type="text" class="tag__edit-input js-tag-edit-input"><button class="tag-edit__btn">Y</button></form> ');
+    return $('<form class="tag__edit js-tag-edit-form">' + 
+            '<span class="tag__edit__input js-tag-edit-input" contenteditable></span>' +
+            '<button class="tag__edit__btn"><img src="tick.svg" alt="Save" /></button>' +
+        '</form> ');
 }
 
 function constructTagElementToDisplay(tagElement) {
@@ -45,11 +92,16 @@ function constructTagElementToDisplay(tagElement) {
     //Add the initially hidden edit form to the tag
     $(tagElement).append(getEditFormHTML());
 
+    // Prevent clicks on the edit form from giving focus to the new tag input.
+    $(tagElement).find('.js-tag-edit-input').on('click', function(evt) {
+        evt.stopPropagation();
+    });
+
     return tagElement;
 }
 
 function getParentTag(childElem) {
-    return $(event.target).parents('.js-tag');
+    return $(childElem).parents('.js-tag');
 }
 
 function updateTagValue($tag, newValue) {
@@ -61,18 +113,45 @@ function getTagValue($tag) {
 }
 
 function startEditingTag($tag) {
+    var $tagInput = $tag.find('.js-tag-edit-input');
+
+    $tagInput.on('keydown', function(evt) {
+        // User hits enter during an edit
+        if (evt.keyCode == 13) {
+            $tag.find('.js-tag-edit-form').submit();
+        }
+        // User Hitting escape
+        if (evt.keyCode == 27) {
+            stopEditingTag($tag);
+        }
+    });
+    
     $tag.addClass('is-editing');
-    $tag.find('.js-tag-edit-input')
-        .val(getTagValue($tag))
-        .select();
+    $tagInput.text(getTagValue($tag));
+    selectElementContents($tagInput[0]);
+    $tagInput[0].focus();
 }
 
 function stopEditingTag($tag) {
     $tag.removeClass('is-editing');
 }
 
-function stopEditingAllTags($container) {
-    $container.find('js-tag').forEach(function() {
-        stopEditingTag(this);
+function stopEditingAllTags() {
+    $('.js-tag').each(function() {
+        stopEditingTag($(this));
+    });
+}
+
+function selectElementContents(el) {
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+}
+
+function removeDuplicateClasses() {
+    $('.js-tag').each(function() {
+        $(this).removeClass('is-duplicate');
     });
 }
